a60320b53cdc3aebcade8419483cf2722071cdff
Arik Smith <ams382@case.edu>
2021-04-25 20:16:14 -0400

    Configure color for App background

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import { Box, Heading } from '@chakra-ui/react'
+
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <h1>Hello World</h1>
-            </header>
-        </div>
-    );
+  return (
+    <Box w="100%" minH="100vh" bg="purple.900" className="App">
+      <header className="App-header">
+        <Heading color="white">Hello World</Heading>
+      </header>
+    </Box>
+  )
 }
 
-export default App;
+export default App
